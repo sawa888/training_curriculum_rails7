06e45997dfc13ddc18c9b891d9ae80ee57c5be9b
sawa888 <oblige.tokyo@docomo.ne.jp>
2024-03-29 21:19:48 +0900

    再修正しました

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -9,7 +9,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -25,7 +24,6 @@ class CalendarsController < ApplicationController
   # def getWeeをスネークケースへ修正
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    wdays[0]
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
